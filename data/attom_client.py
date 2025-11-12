a8d0b2903bed8a0eedbaefeaf5551c2a249683b3
Alec Henderson <alec.henderson@duke.edu>
2025-11-11 21:07:22 -0500

    attom client

--- data/attom_client.py
+++ data/attom_client.py
@@ -9,14 +9,20 @@ ATTOM_API_KEY = os.getenv('ATTOM_API_KEY')
 
 df = pd.read_csv(os.path.join(os.path.join(os.path.dirname(__file__), 'processed'), 'NY_SLI_YEARS_ADJUSTED_NO_UNKNOWNS.csv'))
 
-
+CALL_LIMIT = 1000
 
 def get_attom_property_basicprofile(address1, address2):
     url = f"https://api.gateway.attomdata.com/propertyapi/v1.0.0/property/basicprofile?address1={address1}&address2={address2}"
     headers = {
         "apikey": ATTOM_API_KEY
     }
-    response = requests.get(url, headers=headers)
+    response = None
+    try:
+        response = requests.get(url, headers=headers, timeout=5)
+    except requests.exceptions.RequestException as e:
+        print(f"request failed {e}")
+        return False, None
+    
     if response.status_code == 200:
         data = response.json()
         status = data.get('status', {})
@@ -26,8 +32,6 @@ def get_attom_property_basicprofile(address1, address2):
             return False, data
     else:
         return False, response.json()
-
-
 def getAttomID(json):
     # attomID is first entry in property list since we are calling this for individual houses
     try:
@@ -109,6 +113,18 @@ def getBeds(json):
     except Exception:
         pass
     return None
+def getConstructionQuality(json):
+    try:
+        property_list = json.get('property', [])
+        if isinstance(property_list, list) and property_list:
+            building = property_list[0].get('building', {})
+            construction = building.get('construction', {})
+            quality = construction.get('condition')
+            if beds is not None:
+                return quality
+    except Exception:
+        pass
+    return None
 def getBathsTotal(json):
     try:
         property_list = json.get('property', [])
@@ -145,6 +161,42 @@ def getMarketTotalValue(json):
     except Exception:
         pass
     return None
+def get_sample_split(df, n_samples=20):
+    df = df[df['Service Line Locality'] != 'Pelham Manor'] #only lead here lol
+
+    lead_df = df[df['SL Category Cleaned'] == 'Lead']
+    nonlead_df = df[df['SL Category Cleaned'] != 'Lead']
+
+    lead_locality_counts = lead_df['Service Line Locality'].value_counts()
+    nonlead_locality_counts = nonlead_df['Service Line Locality'].value_counts()
+
+    lead_df = lead_df.set_index('Service Line Locality').loc[lead_locality_counts.index].reset_index()
+    nonlead_df = nonlead_df.set_index('Service Line Locality').loc[nonlead_locality_counts.index].reset_index()
+
+    print(lead_locality_counts)
+    print(nonlead_locality_counts)
+
+    lead_sample = (
+        lead_df.groupby('Service Line Locality')
+        .apply(lambda x:  x.sample(n = n_samples) if (x.shape[0]>n_samples - 1) else x)
+        .reset_index(drop=True)
+        .set_index('Service Line Locality')
+        .loc[lead_locality_counts.index]
+        .reset_index()
+    )
+    nonlead_sample = (
+        nonlead_df.groupby('Service Line Locality')
+        .apply(lambda x:  x.sample(n = n_samples) if (x.shape[0]>n_samples - 1) else x)
+        .reset_index(drop=True)
+        .set_index('Service Line Locality')
+        .loc[lead_locality_counts.index]
+        .reset_index()
+    )
+    return lead_sample, nonlead_sample
+def interleave(a, b):
+    concat_df = pd.concat([a,b]).sort_index().reset_index(drop=True)
+    return concat_df
+
 
 
 i = 0
@@ -161,10 +213,19 @@ print(f"Number of rows in Manhattan: {manhattan_count}")
 
 # df = df.sample(frac=1, random_state=42).reset_index(drop=True)
 
-
 start_time = time.time()
 
-for index, row in df.iterrows():
+# save the raw jsons just in case we want more later
+home_jsons = []
+
+lead_sample, nonlead_sample = get_sample_split(df, n_samples=20)
+
+interleaved_samples = interleave(lead_sample, nonlead_sample)
+
+
+for index, row in interleaved_samples.iterrows():
+    if index > CALL_LIMIT:
+        break
     try:
         print(row['Street Address'] + ' ' + row['Service Line Locality'] + ', ' + row['State'])
         success, json = get_attom_property_basicprofile(
@@ -178,7 +239,7 @@ for index, row in df.iterrows():
         if attomID is None:
             print("-------- could not find any thing!!! ---------")
             continue
-
+        
         lot_size2 = getLotSize2(json)
         prop_class = getPropClass(json)
         year_built = getYearBuilt(json)
@@ -188,7 +249,7 @@ for index, row in df.iterrows():
         baths_total = getBathsTotal(json)
         levels = getLevels(json)
         market_total_value = getMarketTotalValue(json)
-
+        construction_quality = getConstructionQuality(json)
         print(f"attomID: {attomID}")
         print(f"lot_size2: {lot_size2}")
         print(f"prop_class: {prop_class}")
@@ -205,17 +266,19 @@ for index, row in df.iterrows():
         if baths_total == "None" or baths_total is None:
             baths_total = 0.0
 
-        df.at[index, 'attomID'] = attomID
-        df.at[index, 'lot_size2'] = lot_size2
-        df.at[index, 'prop_class'] = prop_class
-        df.at[index, 'year_built'] = year_built
-        df.at[index, 'living_size'] = living_size
-        df.at[index, 'size_ind'] = size_ind
-        df.at[index, 'beds'] = beds
-        df.at[index, 'baths_total'] = baths_total
-        df.at[index, 'levels'] = levels
-        df.at[index, 'market_total_value'] = market_total_value
+        interleaved_samples.at[index, 'attomID'] = attomID
+        interleaved_samples.at[index, 'lot_size2'] = lot_size2
+        interleaved_samples.at[index, 'prop_class'] = prop_class
+        interleaved_samples.at[index, 'year_built'] = year_built
+        interleaved_samples.at[index, 'living_size'] = living_size
+        interleaved_samples.at[index, 'size_ind'] = size_ind
+        interleaved_samples.at[index, 'beds'] = beds
+        interleaved_samples.at[index, 'baths_total'] = baths_total
+        interleaved_samples.at[index, 'levels'] = levels
+        interleaved_samples.at[index, 'market_total_value'] = market_total_value
+        interleaved_samples.at[index, 'construction_condition'] = construction_quality
 
+        home_jsons.append(json)
     except KeyboardInterrupt:
         print("Keyboard interrupt detected. Breaking out of loop.")
         break
@@ -225,9 +288,15 @@ for index, row in df.iterrows():
 end_time = time.time()
 print(f"Loop took {end_time - start_time:.2f} seconds")
 
-print(f"Row count: {len(df)}")
-df = df.dropna(subset=['attomID'])
+print(f"Row count: {len(interleaved_samples)}")
+interleaved_samples = interleaved_samples.dropna(subset=['attomID'])
 
+import json as pyjson
+with open(os.path.join(os.path.dirname(__file__), 'raw/home_jsons.json'), 'w') as f:
+    pyjson.dump(home_jsons, f, indent=2)
 
-df.to_csv(os.path.join(os.path.dirname(__file__), 'processed/NY_SLI_ATTOM.csv'), index=False)
+json_df = pd.DataFrame(home_jsons)
+json_df.to_csv(os.path.join(os.path.dirname(__file__), 'raw/attom_scraped.csv'), index=False)
+
+interleaved_samples.to_csv(os.path.join(os.path.dirname(__file__), 'processed/NY_SLI_ATTOM_NEW.csv'), index=False)
 
